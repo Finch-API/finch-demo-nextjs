36eccc98b1bbfcddfc0bda21d95aaf8e961d8fae
Tyler Nix <github@cometcopy.com>
2022-08-31 11:54:44 -0500

    updating progress bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,31 +1,18 @@
 import Container from './container'
 import NavBar from './navbar'
 import Footer from './footer'
-import NProgress from 'nprogress';
-import useSWR, { SWRConfig } from 'swr'
 
 type LayoutProps = {
   children: React.ReactNode,
 };
 
-async function fetchWithProgress(url: string) {
-  NProgress.start()
-  const data = await fetch(url).then((res) => res.json());
-  NProgress.done()
-  NProgress.remove()
-  return data;
-}
-
 export default function Layout({ children }: LayoutProps) {
   return (
-    <SWRConfig value={{
-      fetcher: fetchWithProgress
-    }}>
-      <div className='layout'>
-        <NavBar />
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </SWRConfig>
+
+    <div className='layout'>
+      <NavBar />
+      <main>{children}</main>
+      <Footer />
+    </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
+import NProgress from 'nprogress';
+import useSWR, { SWRConfig } from 'swr'
+
+async function fetchWithProgress(url: string) {
+  NProgress.start()
+  const data = await fetch(url).then((res) => res.json());
+  NProgress.done()
+  NProgress.remove()
+  return data;
+}
 
 function MyApp({
   Component,
   pageProps: { session, ...pageProps }
 }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SWRConfig value={{
+      fetcher: fetchWithProgress
+    }}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
+
   )
 }
 
