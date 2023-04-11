60cd09457738f80bb56407cb0b47fb74ac305aa3
Tyler Nix <github@cometcopy.com>
2023-04-11 10:13:19 -0500

    working /download/company endpoint

---
+++ pages/api/finch/download/company.tsx
@@ -0,0 +1,148 @@
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { baseUrl, finchApiUrl, sandboxApiUrl } from '../../../../util/constants';
+import database from '../../../../util/database'
+
+export default async function DownloadCompany(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.method + " /api/finch/download/company ");
+
+    if (req.method == 'GET') {
+        try {
+            const token = await database.getConnectionToken()
+            const apiUrl = (await database.isSandbox()) ? sandboxApiUrl : finchApiUrl
+
+            // Get connection details
+            const introspectRes = await axios({
+                method: 'get',
+                url: `${apiUrl}/introspect`,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Finch-API-Version': '2020-09-17'
+                },
+            });
+            const companyId = introspectRes.data.company_id
+            const payrollProviderId = introspectRes.data.payroll_provider_id
+
+            // Get the company details
+            const companyRes = await axios.request<FinchCompany>({
+                method: 'get',
+                url: `${baseUrl}/api/finch/company`,
+            });
+
+
+            // Convert the company data to CSV file
+            const csv = json2csv_company(companyRes.data)
+
+            res.setHeader('Content-Type', 'application/csv');
+            res.setHeader('Content-Disposition', `attachment; filename=finch-${companyId}-${payrollProviderId}-company.csv`);
+            return res.status(200).send(csv);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json("Error downloading Finch company data")
+        }
+    }
+
+    return res.status(405).json("Method not implemented.")
+};
+
+function json2csv_company(CompanyJson: FinchCompany) {
+    const companyHeaders = [
+        "Company ID",
+        "Legal Name",
+        "Entity_type",
+        "Entity_subtype",
+        "EIN",
+        "Primary Email",
+        "Primary Phone Number",
+    ]
+
+    const departmentsHeaders = [
+        "Name",
+        "Parent",
+    ]
+
+    const locationsHeaders = [
+        "Line1",
+        "Line2",
+        "City",
+        "State",
+        "Postal Code",
+        "Country",
+    ]
+
+    const accountsHeaders = [
+        "Institution Name",
+        "Account Name",
+        "Account Number",
+        "Account Type",
+        "Routing Number",
+    ]
+
+    let rows = []
+    rows.push(companyHeaders)
+    rows.push([
+        CompanyJson?.id,
+        CompanyJson?.legal_name?.replace(',', ' '),
+        CompanyJson?.entity.type,
+        CompanyJson?.entity.subtype,
+        CompanyJson?.ein,
+        CompanyJson?.primary_email,
+        CompanyJson?.primary_phone_number,
+    ])
+    rows.push([])
+    rows.push(['Departments'])
+    rows.push(departmentsHeaders)
+    CompanyJson.departments.forEach(dept => {
+        rows.push([dept?.name, dept?.parent.name])
+    })
+    rows.push([])
+    rows.push(['Locations'])
+    rows.push(locationsHeaders)
+    CompanyJson.locations.forEach(loc => {
+        rows.push([
+            loc?.line1,
+            loc?.line2,
+            loc?.city,
+            loc?.state,
+            loc?.postal_code,
+            loc?.country
+        ])
+    })
+    rows.push([])
+    rows.push(['Accounts'])
+    rows.push(accountsHeaders)
+    CompanyJson.accounts.forEach(acc => {
+        rows.push([
+            acc?.institution_name,
+            acc?.account_name,
+            acc?.account_number,
+            acc?.account_type,
+            acc?.routing_number
+        ])
+    })
+
+
+    let csvString = rows.map(row => row.join(',')).join('\n')
+
+    return csvString
+
+
+
+}
+
+function sumAmountsInType(array: any[], type: string | null) {
+    var total = 0
+    array.forEach(element => {
+        if (element.type === type)
+            total += element.amount
+    })
+    return total
+}
+
+function sumTotal(array: any[]) {
+    var total = 0
+    array.forEach(element => {
+        total += element.amount
+    })
+    return total
+}
--- pages/api/finch/download/organization.tsx
+++ pages/api/finch/download/organization.tsx
@@ -1,8 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { baseUrl, finchApiUrl, sandboxApiUrl } from '../../../util/constants';
-import database from '../../../util/database'
-import fs from 'fs'
+import { baseUrl, finchApiUrl, sandboxApiUrl } from '../../../../util/constants';
+import database from '../../../../util/database'
 
 type FinchDirectoryRes = {
     paging: {
@@ -11,104 +10,50 @@ type FinchDirectoryRes = {
     },
     individuals: FinchEmployee[]
 }
-type FinchPayStatementsRes = {
-    responses: FinchPayStatementRes
-}
 
-type FinchPayStatementRes = {
-    payment_id: string,
-    code: number,
-    body: {
-        paging: {
-            count: number,
-            offset: number
-        },
-        pay_statements: FinchPayStatement[]
-    }
-}[]
-
-type PayData = {
-    payment_id: string,
-    pay_date: string,
-    pay_statements: FinchPayStatement[]
-}[]
-
-export default async function Download(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.method + " /api/finch/download ");
-    //const { start_date, end_date } = req.query;
-    const start_date = '2023-01-01'
-    const end_date = '2023-02-01'
-
+export default async function DownloadOrganization(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.method + " /api/finch/download/organization ");
 
     if (req.method == 'GET') {
         try {
             const token = await database.getConnectionToken()
             const apiUrl = (await database.isSandbox()) ? sandboxApiUrl : finchApiUrl
 
-            const directoryRes = await axios.request<FinchDirectoryRes>({
+            // Get connection details
+            const introspectRes = await axios({
                 method: 'get',
-                url: `${baseUrl}/api/finch/directory`,
-                // headers: {
-                //     Authorization: `Bearer ${token}`,
-                //     'Finch-API-Version': '2020-09-17'
-                // },
-            });
-
-            const paymentRes = await axios.request<FinchPayment[]>({
-                method: 'get',
-                url: `${baseUrl}/api/finch/payment?start_date=${start_date}&end_date=${end_date}`,
-                // headers: {
-                //     Authorization: `Bearer ${token}`,
-                //     'Finch-API-Version': '2020-09-17'
-                // },
-            })
-
-            var payData: PayData = [] // we will pass this to the json2csv converter
-            var paymentIds: { payment_id: string }[] = [] // we will use these aggregated ids to call the /pay-statement endpoint
-
-            paymentRes.data.forEach(payment => {
-                payData.push({
-                    payment_id: payment.id,
-                    pay_date: payment.pay_date,
-                    pay_statements: [] // we will add this later once we call the /pay-statements endpoint
-                })
-                paymentIds.push({ payment_id: payment.id })
-            })
-
-            const payStatementRes = await axios.request<FinchPayStatementsRes>({
-                method: 'post',
-                url: `${apiUrl}/employer/pay-statement`,
+                url: `${apiUrl}/introspect`,
                 headers: {
                     Authorization: `Bearer ${token}`,
                     'Finch-API-Version': '2020-09-17'
                 },
-                data: {
-                    requests: paymentIds // batched request of all the payment ids between the start_date and end_date
-                }
-            })
+            });
+            const companyId = introspectRes.data.company_id
+            const payrollProviderId = introspectRes.data.payroll_provider_id
 
-            payStatementRes.data.responses.forEach(response => {
-                var payment = payData.find(payment => payment.payment_id === response.payment_id)
-                if (payment)
-                    payment.pay_statements = response.body.pay_statements // there is a payment_id match, update the pay_statements with the pay_date
-            })
+            // Get the company directory info
+            const directoryRes = await axios.request<FinchDirectoryRes>({
+                method: 'get',
+                url: `${baseUrl}/api/finch/directory`,
+            });
 
 
-            const csv = json2csv(directoryRes.data.individuals, payData)
+            // Convert the organization data to CSV file
+            const csv = json2csv_organization(directoryRes.data.individuals)
 
             res.setHeader('Content-Type', 'application/csv');
-            res.setHeader('Content-Disposition', `attachment; filename=sandbox.csv`);
+            res.setHeader('Content-Disposition', `attachment; filename=finch-${companyId}-${payrollProviderId}-organization.csv`);
             return res.status(200).send(csv);
         } catch (error) {
             console.error(error);
-            return res.status(500).json("Error downloading csv file of Finch data")
+            return res.status(500).json("Error downloading Finch organizational data")
         }
     }
 
     return res.status(405).json("Method not implemented.")
 };
 
-function json2csv(DirectoryJson: FinchEmployee[], PayDataJson: PayData) {
+function json2csv_organization(DirectoryJson: FinchEmployee[]) {
     const headers = [
         "Individual ID",
         "Name",
